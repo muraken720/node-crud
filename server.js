be47a431d863fb59be48f71128a24d9f64b333c5
Kenichiro Murata <kenichiro.murata@gmail.com>
2011-12-15 01:13:46 +0900

    Port & URL change

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.configure(function() {
 
 // Routes
 
-app.get('/', function(req, res) {
+app.get('/memo', function(req, res) {
   console.log("index");
   Memo.find({}, function(err, data) {
     if(err) return next(err);
@@ -65,7 +65,7 @@ app.get('/', function(req, res) {
   });
 });
 
-app.get('/memo', function(req, res, next) {
+app.get('/memo/list', function(req, res, next) {
   console.log("get memos");
   Memo.find({}, function(err, data) {
     if(err) return next(err);
@@ -114,5 +114,5 @@ app.del('/memo/:id', function(req, res, next) {
   });
 });
 
-app.listen(80);
+app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
